a2d31d462fcd8763ef04db6235b5aa516023ae4a
phungbatung <phungbatung2000na@gmail.com>
2025-09-13 13:23:39 +0700

    change sfx dir

---
+++ Assets/Scripts/Utils/Configs.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Configs
+{
+    public enum SFX
+    {
+
+    }
+
+    public enum Music
+    {
+
+    }
+}
